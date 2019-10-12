8435e2e4a6b1145c82029805589d0c994659986f
DevCMJx <cmejia@intellisys.com.do>
2019-10-12 10:00:01 -0400

    loading data

--- routes/views/index.js
+++ routes/views/index.js
@@ -4,6 +4,24 @@ exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+	locals.data = {
+		posts: [],
+	};
+
+	view.on('init', function (next) {
+
+		var q = keystone.list('Post').model.find().limit(6)
+			.sort('-publishedDate');
+
+		if (locals.data.category) {
+			q.where('categories').in([locals.data.category]);
+		}
+
+		q.exec(function (err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+	});
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
